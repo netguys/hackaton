0d41783a9afafb25c92bc12b9e18cf937a84986e
hotrod13 <hotrod.ua@gmail.com>
2014-11-29 16:00:55 +0200

    chart base module

--- client/app/chart/chart.controller.js
+++ client/app/chart/chart.controller.js
@@ -1,6 +1,25 @@
 'use strict';
 
 angular.module('netRankApp')
-  .controller('ChartCtrl', function ($scope) {
+  .controller('ChartCtrl', function ($scope, $window) {
     $scope.message = 'Hello';
+
+    $scope.onSaveButtonClick = function () {
+      var doc = new jsPDF(),
+        elementHandler = {
+          '#ignorePDF': function (element, renderer) {
+            return true;
+          }
+        },
+        source = window.document.getElementById("chart");
+      doc.fromHTML(
+        source,
+        15,
+        15,
+        {
+          'width': 180, 'elementHandlers': elementHandler
+        });
+
+      doc.output('save', 'chart.pdf');
+    }
   });
