0481e6cc672dd8ac65ce2b5b604e9b8ac5581fe3
maksr51314 <maksr51314@gmail.com>
2014-11-29 13:28:59 +0200

    add template

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+  	uri: 'mongodb://myDb:00001234@ds063919.mongolab.com:63919/maksymromanivdb'		
+  },
+
+  seedDB: true
+};
